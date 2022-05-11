ce299c90bc7ef046c8af86fac1b70f63addb81be
thom <hsbang.thom@gmail.com>
2022-05-11 17:04:59 +0900

    commit before merge

---
+++ src/main/java/constructors/DefaultConstructorExample.java
@@ -0,0 +1,15 @@
+package constructors;
+
+public class DefaultConstructorExample {
+
+    private String name;
+    private int value;
+
+    public DefaultConstructorExample(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public DefaultConstructorExample() {
+    }
+}
